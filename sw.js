b93f1d1833bc779b632ba04204d0346b7fc8f6e1
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-26 07:28:47 +0000

    Deploy mazipan/quran-offline to github.com/mazipan/quran-offline.git:gh-pages

--- sw.js
+++ sw.js
@@ -28,7 +28,16 @@ workbox.precaching.precacheAndRoute([
   "/star-logo-color-64.png",
   "/star-logo-color.png",
   "/icon.png",
-  "/read-quran.svg"
+  "/read-quran.svg",
+  "/icon_allah.svg",
+  "/icon_clock.svg",
+  "/icon_prayer.svg",
+  "/icon_quran.svg",
+  "/icon_ramadhan.svg",
+  "/icon_rosary.svg",
+  "/illustration_1.jpg",
+  "/illustration_2.jpg",
+  "/illustration_3.jpg"
 ], {
   "cacheId": "quran-offline-prod",
   "directoryIndex": "/"
