feed7698d051decc54afeb3e664e1f99ee8a0234
irfan-maulana-tkp <irfan-maulana-tkp@users.noreply.github.com>
2020-05-02 22:34:25 +0000

    deploy: ba9e428019563f4a026d950f1099d350a6b6a970

--- sw.js
+++ sw.js
@@ -31,14 +31,16 @@ workbox.precaching.precacheAndRoute([
   "/read-quran.svg",
   "/icon_allah.svg",
   "/icon_clock.svg",
+  "/icon_mosque.svg",
   "/icon_prayer.svg",
   "/icon_quran.svg",
   "/icon_ramadhan.svg",
   "/icon_rosary.svg",
   "/illustration_1.jpg",
   "/illustration_2.jpg",
-  "/illustration_3.jpg",
-  "/fonts/lpmq.otf"
+  "/meta-image.png",
+  "/fonts/lpmq.otf",
+  "/fonts/lpmq.woff"
 ], {
   "cacheId": "quran-offline-prod",
   "directoryIndex": "/"
